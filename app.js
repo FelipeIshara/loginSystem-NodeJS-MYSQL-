b32c9dd84e66c19ecd5d69c98582ac2cc7dbf781
SEUMERDA <felipeishara@hotmail.com>
2020-09-28 09:13:09 -0300

    db connection

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database:'nodejs-login'
+})
+
+db.connect((error) => {
+    if(error) {
+        console.log(error)
+    }
+    else{
+        console.log("MySQL Connected...")
+    }
+})
 
 app.get('/', (req,res) => {
     res.send('hello world')
